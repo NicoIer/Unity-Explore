4c8ba5c11e01bcb17a77151f96fe4e7fa36ee008
NicoIer <2320595857@qq.com>
2023-07-16 22:50:22 +0800

    feat: update

---
+++ Assets/Games/VampireSurvivors/Assemblies/Character/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Nico;
+using UnityEngine;
+
+namespace VampireSurvivors
+{
+    public class Player : SceneSingleton<Player>
+    {
+        public PlayerSetting setting;
+        private PlayerInputManager _inputManager;
+        private MoveComponent _moveComponent;
+        public List<ShooterComponent> shooterComponents;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _inputManager = new PlayerInputManager();
+
+            shooterComponents = new List<ShooterComponent>();
+            ShooterComponent shooterComponent = new ShooterComponent(setting.shooterSetting, FindEnemy);
+            shooterComponents.Add(shooterComponent);
+
+            _moveComponent = new MoveComponent(transform)
+            {
+                speed = setting.speed
+            };
+        }
+        private void OnEnable()
+        {
+            _inputManager.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputManager.Disable();
+        }
+
+        private void Update()
+        {
+            _moveComponent.move = _inputManager.input.Player.Move.ReadValue<Vector2>();
+            _moveComponent.OnUpdate();
+
+            foreach (var shooterComponent in shooterComponents)
+            {
+                shooterComponent.Shoot(transform.position);
+            }
+        }
+        
+        private Transform FindEnemy()
+        {
+            Enemy enemy = Enemy.enemies.Random();
+            if (enemy != null)
+            {
+                return enemy.transform;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Games/VampireSurvivors/Assemblies/Weapon/ShooterComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Nico;
+using UnityEngine;
+
+namespace VampireSurvivors
+{
+    public class ShooterComponent
+    {
+        private readonly ShooterSetting _setting;
+        private bool _canShoot;
+        private readonly Func<Transform> _findTarget;
+        public ShooterComponent(ShooterSetting setting,Func<Transform> findTarget)
+        {
+            this._setting = setting;
+            this._findTarget = findTarget;
+            _canShoot = true;
+        }
+        public async void Shoot(Vector3 start)
+        {
+            if (!_canShoot)
+            {
+                return;
+            }
+            Transform tar = _findTarget();
+            if (tar != null)
+            {
+                GameObject bulletObj = ObjectPoolManager.Get(_setting.bulletName);
+                bulletObj.transform.position = start;
+                Bullet bullet = bulletObj.GetComponent<Bullet>();
+                bullet.Init(tar);
+            }
+            _canShoot = false;
+            await UniTask.Delay(TimeSpan.FromSeconds(_setting.coolDown));
+            _canShoot = true;
+        }
+    }
+}
