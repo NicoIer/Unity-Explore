2da81d53df61d28c404bd5fbd3d491c414b1cb77
NicoIer <2320595857@qq.com>
2023-07-16 23:11:57 +0800

    feat: demo test

---
+++ Assets/Games/OneFingerDeathPunch/Assemblies/VirtualStickListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nico.VirtualControl;
+using UnityEngine;
+
+namespace OneFingerDeathPunch
+{
+    public class VirtualStickListener: MonoBehaviour
+    {
+        public VirtualStick stick;
+        public float speed = 5f;
+
+        public void Update()
+        {
+            Vector2 move = stick.ReadAxis();
+            transform.position += new Vector3(move.x, move.y, 0) * (Time.deltaTime * speed);
+        }
+    }
+}
